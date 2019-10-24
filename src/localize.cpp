6a99119083e37c9d1c90e2b2faef313728f38035
Supasan Komonlit <supasan_224@hotmail.com>
2019-10-24 12:44:02 +0700

    Init package

---
+++ src/localize.cpp
@@ -0,0 +1,16 @@
+// FILE			: localize.cpp
+// AUTHOR		: K.Supasan
+// CREATE ON	: 2019, October 24 (UTC+0)
+// MAINTAINER	: K.Supasan
+
+// MACRO DETAIL
+
+// README
+
+// REFERENCE
+
+// MACRO SET
+
+// MACRO CONDITION
+
+
