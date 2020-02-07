d27f4a9f5f9f8ed365b7e79681068467e7be2a51
SupasanKomonlit <supasan_224@hotmail.com>
2020-02-07 22:05:58 +0700

    Start compile mapper

---
+++ include/mapper.hpp
@@ -0,0 +1,48 @@
+// FILE			: mapper.hpp
+// AUTHOR		: K.Supasan
+// CREATE ON	: 2020, Febuary 07 (UTC+0)
+// MAINTAINER	: K.Supasan
+
+// MACRO DETAIL
+
+// README
+
+// REFERENCE
+
+// MACRO SET
+
+// MACRO CONDITION
+
+#include    <mutex>
+
+#include    <iostream>
+
+#include    <nav_msgs/Odometry.h>
+
+#include    <zeabus/ros/node.hpp>
+
+#include    <zeabus/ros/convert/geometry_quaternion.hpp>
+
+#include    <zeabus/ros/subscriber/base_class.hpp>
+
+#include    <zeabus/ros/path_file.hpp>
+
+#include    <zeabus/ros/static_tf.hpp>
+
+#include    <tf/LinearMath/Quaternion.h>
+
+#include    <tf/transform_broadcaster.h>
+
+#include    <tf/transform_listener.h>
+
+#include    <ros/ros.h>
+
+#include    <zeabus_utility/Float64Array8.h>
+
+#include    <zeabus/robot.hpp>
+
+#include    <boost/qvm/vec_access.hpp>
+
+#include    <boost/qvm/mat_access.hpp> 
+
+#include    <zeabus/file.hpp>
---
+++ src/mapper/mapper.cpp
@@ -0,0 +1,53 @@
+// FILE			: mapper.cpp
+// AUTHOR		: K.Supasan
+// CREATE ON	: 2020, Febuary 07 (UTC+0)
+// MAINTAINER	: K.Supasan
+
+// MACRO DETAIL
+
+// README
+
+// REFERENCE
+
+// MACRO SET
+
+// MACRO CONDITION
+
+#include    <mapper.hpp>
+
+int main( int argv , char** argc )
+{
+    zeabus_ros::Node node( argv , argc , "visual_map" );
+
+    ros::NodeHandle ph( "~" );
+    ros::NodeHandle nh( "" );
+
+    node.spin();
+
+    // Part variable inital setup
+    zeabus::FileCSV fh; // file module to collect mapper
+    zeabus_ros::StaticTF sth; // static transformation handle
+    std::string file_name;
+
+
+    ros::Rate rate( 30 );
+    // Part setup parameter variable for use in ros system
+    ph.param< std::string >( "file_map" , file_name , "qualification01.txt" );
+
+
+    // Part setup ros variable to activate ros system
+    fh.open( zeabus_ros::get_full_path( "zeabus_localize" , "parameter/map" , file_name ) );
+    ( void )sth.setup( &fh );
+
+    sth.spin( 10 );
+
+active_main:
+    while( ros::ok() )
+    {
+        rate.sleep();
+    }
+
+exit_main:
+    return 0;
+    
+}
